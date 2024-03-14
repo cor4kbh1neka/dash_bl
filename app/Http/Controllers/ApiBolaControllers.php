4434bc197a17abb761dc318ac2cb5bdbe28e6a6d
wantos <irwanzzz65@gmail.com>
2024-03-14 19:31:34 +0700

    test

--- app/Http/Controllers/ApiBolaControllers.php
+++ app/Http/Controllers/ApiBolaControllers.php
@@ -50,4 +50,77 @@ class ApiBolaControllers extends Controller
     {
         return '/ReturnStake';
     }
+
+    public function login($username, $iswap)
+    {
+        try {
+            $dataLogin['Username'] = $username;
+            $dataLogin['CompanyKey'] = env('COMPANY_KEY');
+            $dataLogin['Portfolio'] = env('PORTFOLIO');
+            $dataLogin['IsWapSports'] = $iswap;
+            $dataLogin['ServerId'] = "YY-TEST";
+            $getLogin = $this->requestApiLogin($dataLogin);
+            return $getLogin;
+        } catch (\Exception $e) {
+            return $this->errorResponse($username, 99, $e->getMessage());
+        }
+    }
+
+    function requestApiLogin($data)
+    {
+        $url = 'https://ex-api-demo-yy.568win.com/web-root/restricted/player/login.aspx';
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json; charset=UTF-8',
+        ])->post($url, $data);
+
+        if ($response->successful()) {
+            $responseData = $response->json();
+        } else {
+            $statusCode = $response->status();
+            $errorMessage = $response->body();
+            $responseData = "Error: $statusCode - $errorMessage";
+        }
+        return ['url' => $responseData["url"]];
+    }
+
+    public function register(Request $request)
+    {
+        if (!$request->header('Authorization')) {
+            return response()->json(['message' => 'Unauthorized.'], 401);
+        }
+
+        $data = [
+            "Username" => $request->Username,
+            "UserGroup" => "c",
+            "Agent" => "Agent_C_001",
+            "CompanyKey" => env('COMPANY_KEY'),
+            "ServerId" => "YY-TEST"
+        ];
+
+        $url = 'https://ex-api-demo-yy.568win.com/web-root/restricted/player/register-player.aspx';
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json; charset=UTF-8',
+            'Authorization' => 'Bearer ' .  env('BEARER_TOKEN'),
+        ])->post($url, $data);
+
+        if ($response->successful()) {
+            $responseData = $response->json();
+        } else {
+            $statusCode = $response->status();
+            $errorMessage = $response->body();
+            $responseData = "Error: $statusCode - $errorMessage";
+        }
+
+        if ($responseData["error"]["id"] === 0) {
+            return response()->json([
+                'message' => 'Data berhasil disimpan.'
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => $responseData["error"]["msg"] ?? 'Error tidak teridentifikasi.'
+            ], 400);
+        }
+    }
 }
