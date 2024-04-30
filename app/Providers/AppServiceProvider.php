c771cfaefd07d102c6436dd9823b1687cfb4c2dc
wantos <irwanzzz65@gmail.com>
2024-04-30 22:02:20 +0700

    as23c1sa23c1sa23c1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,32 +29,32 @@ class AppServiceProvider extends ServiceProvider
 
     private function getDataCount()
     {
-        // $countDataDP = Xdpwd::where('jenis', 'DP')->where('status', 0)->count();
-        // $countDataWD = Xdpwd::where('jenis', 'WD')->where('status', 0)->count();
+        $countDataDP = Xdpwd::where('jenis', 'DP')->where('status', 0)->count();
+        $countDataWD = Xdpwd::where('jenis', 'WD')->where('status', 0)->count();
 
-        // $dataOuts = Outstanding::get();
-        // $dataOuts = $dataOuts->groupBy('username')->map(function ($group) {
-        //     $totalAmount = $group->sum('amount');
-        //     $count = $group->count();
-        //     return [
-        //         'username' => $group->first()['username'],
-        //         'totalAmount' => $totalAmount,
-        //         'count' => $count,
-        //     ];
-        // })->count();
+        $dataOuts = Outstanding::get();
+        $dataOuts = $dataOuts->groupBy('username')->map(function ($group) {
+            $totalAmount = $group->sum('amount');
+            $count = $group->count();
+            return [
+                'username' => $group->first()['username'],
+                'totalAmount' => $totalAmount,
+                'count' => $count,
+            ];
+        })->count();
 
-        // $responseMemo = Http::get('https://back-staging.bosraka.com/memo');
-        // $resultMemo = $responseMemo->json();
-        // if ($resultMemo['status'] == 'success') {
-        //     $countMemo = count($resultMemo['data']);
-        // }
+        $responseMemo = Http::get('https://back-staging.bosraka.com/memo');
+        $resultMemo = $responseMemo->json();
+        if ($resultMemo['status'] == 'success') {
+            $countMemo = count($resultMemo['data']);
+        }
 
 
-        // return [
-        //     'countDP' => $countDataDP,
-        //     'countWD' => $countDataWD,
-        //     'countOuts' => $dataOuts,
-        //     'countMemo' => $countMemo
-        // ];
+        return [
+            'countDP' => $countDataDP,
+            'countWD' => $countDataWD,
+            'countOuts' => $dataOuts,
+            'countMemo' => $countMemo
+        ];
     }
 }
