75adb986f31a058a05616d6fffc14f3314ce3877
wantos <irwanzzz65@gmail.com>
2024-05-13 16:25:12 +0700

    test

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\Cors::class,
     ];
 
     /**
@@ -64,7 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'allowedIP' => \App\Http\Middleware\CheckAllowedIPMiddleware::class,
-        'check.domain' => \App\Http\Middleware\CheckDomain::class
+        'allowedIP' => \App\Http\Middleware\CheckAllowedIPMiddleware::class
     ];
 }
